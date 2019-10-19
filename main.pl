95736135dd8dcbbbd6378375f15a4f4b84997660
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2019-10-19 14:59:46 +0200

    Positive to negative words within the same word class

--- main.pl
+++ main.pl
@@ -1,16 +1,37 @@
 :- use_module(library(apply)).
+:- use_module(library(lists)).
 
-novel("att vara ganska tragisk").
+novel("att vara ganska tragisk är en väldig synd").
 
-% Random word with the inverse sentimental score
+% Converts positive words to negative ones if defined.
+positive_to_negative() :- false.
+
+% Clamps the value to one side of 0
+clamp(Value, NewValue) :-
+    not(positive_to_negative()),
+    (integer(Value) > 0 ->
+         NewValue is  integer(Value) ;
+         NewValue is -integer(Value)).
+
+clamp(Value, NewValue) :-
+    positive_to_negative(),
+    (integer(Value) > 0 ->
+         NewValue is -integer(Value) ;
+         NewValue is  integer(Value)).
+
+% Random word with the same class and inverse sentimental score
 word_analysis(Element, NewWord) :-
     string_lower(Element, LowerCase),
     word_strength(LowerCase, Strength),
+    clamp(Strength, InvStrength),
     InvStrength is -integer(Strength),
-    findall(NewWord, word_strength(NewWord, InvStrength), NewWords),
+    findall(NewWord, word_strength(NewWord, InvStrength), StrengthWords),
+    word_class(LowerCase, WordClass),
+    findall(NewWord, word_class(NewWord, WordClass), ClassWords),
+    intersection(StrengthWords, ClassWords, NewWords),
     random_member(NewWord, NewWords).
 
-% Word which does not exist in the database
+% Words which do not exist in the database
 word_analysis(Element, Element).
 
 p() :-
