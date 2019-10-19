115a4f69de106a4795fbb7af556b1f101178e551
Alexander Koski <akoski@kth.se>
2019-10-19 13:52:06 +0200

    Add wordclass in db

---
+++ README.md
@@ -0,0 +1,13 @@
+# Jajjamensan
+
+## Ordklasser:
+
+"av" - Adverb
+
+"vb" - Verb
+
+"ab" - Adverb
+
+"vbm" - Sammandatt fras
+
+"avm" - Sammansatt fras
--- word_lexicons/csv_cleaner.py
+++ word_lexicons/csv_cleaner.py
@@ -4,25 +4,39 @@ fd_out_name = sys.argv[1]
 
 fd = open("sentimentlex.csv", "r")
 fd_out = open(fd_out_name, "w")
+lines = fd.readlines()[1:]
 
 # State words
-lines = fd.readlines()[1:]
 for line in lines:
-    word = line.split(",")[0]#.replace("\"", "")
+    word = line.split(",")[0]
     if " " in word:
         continue
-    # strength = line.split(",")[2]
     fd_out.write("word(" + word + ")" + ".\n")
+
+
+# State word and its wordclass
+#wcl = []
+for line in lines:
+    word = line.split(",")[0]
+    word_class = line.split(",")[5].replace("\"", "")
+    # if word_class not in wcl:
+    #     wcl.append(word_class)
+    if " " in word:
+        continue
+    fd_out.write("word_class(" + word + ", " + word_class + ")" + ".\n")
+
+# for i in wcl:
+#     print(i)
 
 
 # State words and its strengts
 for line in lines:
-    word = line.split(",")[0]#.replace("\"", "")
+    word = line.split(",")[0]
     if " " in word:
         continue
     strength = line.split(",")[2]
     fd_out.write("word_strength(" + word + ", " + strength + ")" + ".\n")
-fd_out.write("word_strength(" + "_" + ", " + str(0) + ")" + ",!.\n")
+
 
 fd.close()
 fd_out.close()
